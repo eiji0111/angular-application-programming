447d66f13ec4ab6f63531346471be17aed888be7
eiji0111 <ee111fender@gmail.com>
2021-08-22 17:52:54 +0900

    [Update]web-animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule /*オブジェクト名*/ } from '@angular/core'; // モジュ
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { CoopModule } from './coop/coop.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child.component';
 
 @NgModule({
-  imports: [BrowserModule, CoopModule],
+  imports: [BrowserModule, CoopModule, BrowserAnimationsModule],
   declarations: [AppComponent, ChildComponent],
   bootstrap: [AppComponent] // ルートコンポーネント・メインコンポーネントという。
 })

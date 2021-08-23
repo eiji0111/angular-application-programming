cc6187f32413e3252a90703bdbcd7ffee8ad9276
eiji0111 <ee111fender@gmail.com>
2021-08-23 22:56:25 +0900

    [Fix]tsconfig.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,42 @@
-import { Component, OnChanges, OnInit, DoCheck,
-  AfterContentInit, AfterContentChecked, AfterViewInit,
-  AfterViewChecked, OnDestroy } from '@angular/core';
-import { style } from '@angular/core/src/animation/dsl';
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/core/src/animation/dsl';
 
-// 構成を宣言する
 @Component({
   selector: 'my-app',
-  template: `
-    <div>
-      <label>表示／非表示
-        <input type="checkbox" (change)="onchange()" checked />
-      </label>
-    </div>
-    <my-child [time]="current" *ngIf="show"></my-child>
-  `,
-  styles: [`
-    :host >>> p {
-      display: block;
-      border: 1px double Red;
-    }`]
+  animations: [
+    trigger('btnState', [
+      state('off', style({
+        border: 'none',
+        backgroundColor: '#000',
+        color: '#fff',
+        fontWeight: 'normal',
+        transform: 'scale(0.8) rotate(0deg)'
+      })),
+      state('on',   style({
+        border: 'solid 1px #fff',
+        backgroundColor: '#f00',
+        color: '#fff',
+        fontWeight: 'bold',
+        transform: 'scale(1) rotate(5deg)'
+      })),
+      transition('off => on', animate('200ms linear')),
+      transition('on => off', animate('200ms linear'))
+      
+      //transition関数の省略形
+      //transition('off => on, on => off', animate('200ms linear'))
+      //transition('off <=> on', animate('200ms linear')),
+    ])
+  ],
+  template: `<input type="button" [value]="caption"
+    [@btnState]="flag" (click)="toggle()" />`,
+  styles:[` input{ width: 100px; height: 50px}`]
 })
+export class AppComponent  {
+  flag = 'off';
+  caption = 'オフ'
 
-// クラス名
-export class AppComponent implements OnChanges,
-  OnInit, DoCheck, AfterContentInit, AfterContentChecked,
-  AfterViewInit, AfterViewChecked, OnDestroy {
-  show = true;
-  current = new Date();
-
-  // チェックボックス変更時に実行
-  onchange() {
-    this.show = !this.show;
-    this.current = new Date();
-  }
-
-  constructor() {
-    console.log('constructor');
-  }
-
-  // ライフサイクルメソッド
-  ngOnInit() {
-    console.log('ngOnInit');
-  }
-  ngOnChanges() {
-    console.log('ngOnChanges');
-  }
-  ngDoCheck() {
-    console.log('ndDoCheck');
-  }
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit');
-  }
-  ngAfterContentChecked() {
-    console.log('ngAfterContentChecked');
-  }
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-  }
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked');
-  }
-  ngOnDestroy() {
-    console.log('ngOnDestroy');
+  toggle() {
+    this.flag = (this.flag === 'on' ? 'off' : 'on');
+    this.caption = (this.caption === 'オン' ? 'オフ' : 'オン');
   }
 }
-
-// 片方向バインディング [] ()
-// 双方向バインディング [()]
